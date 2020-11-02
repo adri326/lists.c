63f264bd58ffde74a77089a6b4fdf4735f156fd3
Adrien Burgun <adrien.burgun@orange.fr>
2020-11-02 08:15:59 +0100

    :bug: Fix broken include.cmake

--- include.cmake
+++ include.cmake
@@ -2,7 +2,7 @@ cmake_minimum_required(VERSION 3.15)
 
 set(LISTS_C_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/src")
 
-set(LISTS_C_SOURCES src/linkedlist.c)
-set(LISTS_C_HEADERS src/linkedlist.h)
+set(LISTS_C_SOURCES "${CMAKE_CURRENT_LIST_DIR}/src/linkedlist.c")
+set(LISTS_C_HEADERS "${CMAKE_CURRENT_LIST_DIR}/src/linkedlist.h")
 
 add_library(lists-c ${LISTS_C_SOURCES} ${LISTS_C_HEADERS})
